bd2c454737ce0dd3127f6f0c0a63728e950667b1
GaelVM <ajhuneor@outlook.com>
2023-10-14 12:28:32 -0500

    sd

--- detailedscrape.js
+++ detailedscrape.js
@@ -13,7 +13,7 @@ function main()
 
     var events = JSON.parse(fs.readFileSync("./files/events.min.json"));
 
-    https.get("https://raw.githubusercontent.com/bigfoott/ScrapedDuck/data/events.min.json", (res) =>
+    https.get("https://raw.githubusercontent.com/GaelVM/DataDuck/data/events.min.json", (res) =>
     {
         let body = "";
         res.on("data", (chunk) => { body += chunk; });
