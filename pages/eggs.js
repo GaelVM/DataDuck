89bc1b3dfa29ba7ddfec78a6c3858aaf346cf0dd
GaelVM <ajhuneor@outlook.com>
2023-10-14 00:16:50 -0500

    aa

--- pages/eggs.js
+++ pages/eggs.js
@@ -71,7 +71,7 @@ function get()
         }).catch(_err =>
             {
                 console.log(_err);
-                https.get("https://raw.githubusercontent.com/bigfoott/ScrapedDuck/data/eggs.min.json", (res) =>
+                https.get("https://raw.githubusercontent.com/GaelVM/DataDuck/data/eggs.min.json", (res) =>
                 {
                     let body = "";
                     res.on("data", (chunk) => { body += chunk; });
